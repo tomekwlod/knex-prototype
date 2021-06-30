76ca38a2d51628e01cdf94fe695a316abea9fd67
Tomasz Wlodarczyk <tomek.wlod@gmail.com>
2021-06-30 15:28:56 +0100

    jest update

--- test/knex/postgres/postgres.test.js
+++ test/knex/postgres/postgres.test.js
@@ -35,93 +35,113 @@ const clear = async () => {
     await manc.raw({}, `truncate many`);
 };
 
-it('knex - wrong manager', async done => {
+it('knex - wrong manager', done => {
 
-    try {
+    (async function() {
+        try {
 
-        knex('pg').model.nonexisting.test();
-    }
-    catch (e) {
+            knex('pg').model.nonexisting.test();
+        }
+        catch (e) {
 
-        expect(String(e)).toBe("Error: No such pg manager 'nonexisting', registered managers are: common, users, many, wrongTest");
+            expect(String(e)).toBe("Error: No such pg manager 'nonexisting', registered managers are: common, users, many, wrongTest");
+
+            done();
+        }
+    }())
+});
+
+it('knex - postgres', done => {
+
+    (async function() {
+        const list = await man.query({}, 'SELECT datname FROM pg_database WHERE datistemplate = false;');
+
+        let tmp = list.map(x => Object.values(x)[0]);
+
+        const db = process.env.PROTECTED_PG_DB;
+
+        if (db) {
+
+            const found = tmp.find(x => x === db);
+
+            // man.destroy();
+
+            expect(found).toEqual(db);
+
+            done();
+        }
+
+    }())
+});
+
+it(`knex - postgres - init`, done => {
+    
+    (async function() {
+
+        const init = await manc.initial({});
+
+        expect(init).toEqual({prototype:'MYSQL: prototype.initial()'});
 
         done();
-    }
+    }())
+    
 });
 
-it('knex - postgres', async done => {
+it(`knex - postgres - fromDb`, done => {
 
-    const list = await man.query({}, 'SELECT datname FROM pg_database WHERE datistemplate = false;');
+    (async function() {
+        const init = await manc.fromDb({}, [{test: true}]);
 
-    let tmp = list.map(x => Object.values(x)[0]);
-
-    const db = process.env.PROTECTED_PG_DB;
-
-    if (db) {
-
-        const found = tmp.find(x => x === db);
-
-        // man.destroy();
-
-        expect(found).toEqual(db);
+        expect(init).toEqual([{test: true}]);
 
         done();
-    }
+    }())
 });
 
-it(`knex - postgres - init`, async done => {
+it(`knex - postgres - toDb`, done => {
 
-    const init = await manc.initial({});
+    (async function() {
+        const init = await manc.toDb({}, {test: true});
 
-    expect(init).toEqual({prototype:'MYSQL: prototype.initial()'});
+        expect(init).toEqual({test: true});
 
-    done();
+        done();
+    }())
 });
 
-it(`knex - postgres - fromDb`, async done => {
+it(`knex - postgres - queryColumn, array params`, done => {
 
-    const init = await manc.fromDb({}, [{test: true}]);
+    (async function() {
+        const lastName = await man.queryColumn({}, 'select "lastName" from :table: u where u.:id: = ?', [1]);
 
-    expect(init).toEqual([{test: true}]);
+        expect(lastName).toEqual('admin');
 
-    done();
+        done();
+    }())
+    
 });
 
-it(`knex - postgres - toDb`, async done => {
+it(`knex - postgres - queryColumn, array params, one param is also array`, done => {
 
-    const init = await manc.toDb({}, {test: true});
+    (async function() {
+        const data = await man.query({}, 'select "lastName" from :table: u where u.:id: in (?)', [[1, 2]]);
 
-    expect(init).toEqual({test: true});
+        expect(data).toEqual([
+            {"lastName": "admin"},
+            {"lastName": "user"}
+        ]);
 
-    done();
+        done();
+    }())
 });
 
-it(`knex - postgres - queryColumn, array params`, async done => {
-
-    const lastName = await man.queryColumn({}, 'select "lastName" from :table: u where u.:id: = ?', [1]);
-
-    expect(lastName).toEqual('admin');
-
-    done();
-});
-
-it(`knex - postgres - queryColumn, array params, one param is also array`, async done => {
-
-    const data = await man.query({}, 'select "lastName" from :table: u where u.:id: in (?)', [[1, 2]]);
-
-    expect(data).toEqual([
-        {"lastName": "admin"},
-        {"lastName": "user"}
-    ]);
-
-    done();
-});
-
-// it(`knex - postgres - count`, async done => {
+// it(`knex - postgres - count`, done => {
 //
+// (async function() {
 //     const data = await man.count({});
 //
 //     expect(data).toEqual(2);
 //
 //     done();
+// }())
 // });
